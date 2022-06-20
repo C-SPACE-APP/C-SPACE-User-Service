37e74a357adfb9839aa468b3d31d4f768f578dca
bsfelizardo <bsfelizardo@up.edu.ph>
2022-06-20 22:04:12 +0800

    Update authorization middelware
    -	Add option for limiting access to owner of data
    -	Will check if ID of logged in user is the same as ID of user in
    	data being accessed

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -1,11 +1,23 @@
-module.exports = () => {
+module.exports = (type) => {
     return (async (req, res, next) => {
-       if(!req.session.User) {
-        return res.status(401).json({
-            message: `Not logged in`
-        })
-       }
 
-       next()
+        /**
+         * Check if logged in
+         */
+        if(!req.session.User) {
+            return res.status(401).json({ message: `Not logged in` })
+        }
+
+        /**
+         * Check if logged in user owns data to be accessed
+         */
+        if(type === 'OWNER') {
+            const { _id } = req.session.User
+            const { id } = req.params
+
+            if(_id !== id) return res.status(403).json({ message: `User ${_id} unauthorized accessed on ${id}`})
+        }
+
+        next()
     })
 }
