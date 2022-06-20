89eea3da2d1b5cb1fe47cd3e8a7a9c21264562cf
bsfelizardo <bsfelizardo@up.edu.ph>
2022-06-20 21:56:25 +0800

    Add function for sanitizing object properties -	will remove object properties that are null or undefined -	will remove object properties that contains only whitespaces -	will return a sanitized object

--- src/database/utils.js
+++ src/database/utils.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 
 class DBUtils {
 
+    constructor() {
+        this.BLANK_PATTERN = /\S/g
+    }
+
     async validID(id) {
         try { 
             const validID = mongoose.Types.ObjectId(id)
@@ -9,6 +13,22 @@ class DBUtils {
         } catch(err) {
             return false
         }
+    }
+
+    async sanitize(obj) {
+        for(const key in obj) {
+            if(!obj[key]) {
+                delete obj[key]
+                continue
+            }
+
+            if(typeof obj[key] === 'string' && !obj[key].match(this.BLANK_PATTERN)) {
+                delete obj[key]
+                continue
+            }
+        }
+        
+        return obj
     }
     
 }
