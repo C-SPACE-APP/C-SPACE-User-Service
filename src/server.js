352d1036a6e313cbd2e11955dfecbb9484767c04
bsfelizardo <bsfelizardo@up.edu.ph>
2022-06-27 18:18:35 +0800

    Update allowed origin

--- src/server.js
+++ src/server.js
@@ -7,7 +7,17 @@ const { Authenticate } = require('./middlewares')
 module.exports = async (app) => {
     app.use(cors({
         credentials: true,
-        origin: 'http://localhost:3000'
+        origin: [
+            'http://localhost:3000', 
+            'http://localhost:3001', 
+            'http://localhost:3002',
+            'http://localhost:3003',
+            'http://localhost:3004',
+            'http://localhost:3005',
+            'http://localhost:3006',
+            'http://localhost:3007',
+            'http://localhost:3008'
+        ]
     }))
     app.use(express.urlencoded({extended: true}));
     app.use(express.json())
