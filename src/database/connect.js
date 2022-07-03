832f37519146875ea6401c2bf35350d4c62774cc
bsfelizardo <bsfelizardo@up.edu.ph>
2022-07-03 11:48:21 +0800

    Update connection string

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,11 +1,9 @@
-const mongoose = require('mongoose')
-
 require('dotenv').config()
 
-module.exports = async () => {
+module.exports = async (database) => {
     try {
-        await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}${process.env.URL}${process.env.QUERY}`,
+        await database.connect(
+            `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
             { useNewUrlParser: true, useUnifiedTopology: true }
         )
         console.log(`Connected to Database`);
